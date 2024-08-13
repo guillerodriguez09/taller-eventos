314d52d3fe37090fc54252b8a1132ab1174fff47
guillerodriguez09 <rodriguezguillermina816@gmail.com>
2024-08-13 15:12:30 -0300

    jaja

--- events.js
+++ events.js
@@ -6,6 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     let container= document.getElementById('container');
     container.addEventListener('click', function(){
         alert('Hola! Soy el div jiji')
-    } )
+    } );
 }
-)
+);
